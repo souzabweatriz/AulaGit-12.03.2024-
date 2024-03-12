ab89a67bf5805515582109fe3993e684dc5acb1f
Ana Beatriz <anasouza.tds1.senai@gmail.com>
2024-03-12 11:08:05 -0300

    aaaa

--- adivinhação.js
+++ adivinhação.js
@@ -3,7 +3,7 @@ let numeroaleatorio = (Math.floor(Math.random(1) * 100));
 let pessoa = (Math.floor(Math.random(1) * 100));
 
 
-let tentativa = 6;
+let tentativa = 8;
 
 while( numeroaleatorio != pessoa ){
     pessoa = Math.floor(Math.random(1) * 100);
